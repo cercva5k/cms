dbfee124f0343910d5f9fd1e61acc95160020a3e
Tornike Tsertsvadze <goart2000@gmail.com>
2020-10-24 01:01:27 +0400

    feat: git ignore and loader

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.version();
+mix.setPublicPath('public');
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/styles/app.sass', 'public/css');
+
